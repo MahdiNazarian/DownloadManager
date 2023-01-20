15338c244c250a0e1fac75a07db6b96b46615f45
MahdiNazarian <62932091+MahdiNazarian@users.noreply.github.com>
2023-01-20 21:31:54 +0330

    register added

--- app/src/main/java/com/ghazalpaknia_mahdinazarian/downloadmanager/MainActivity.kt
+++ app/src/main/java/com/ghazalpaknia_mahdinazarian/downloadmanager/MainActivity.kt
@@ -2,13 +2,17 @@ package com.ghazalpaknia_mahdinazarian.downloadmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.FrameLayout
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.FragmentContainerView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.ghazalpaknia_mahdinazarian.Dialoges.RegisterUserBottomSheetDialog
 import com.ghazalpaknia_mahdinazarian.app_models.DownloadsItemModel
 import com.ghazalpaknia_mahdinazarian.recylcler_view_adapters.DownloadListItemsAdapter
 import com.ghazalpaknia_mahdinazarian.static_values.StaticValues
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +21,14 @@ class MainActivity : AppCompatActivity() {
     }
     fun onListsClick(){
 
+    }
+
+    fun onOpenDrawerClick(view: View) {
+        val drawer: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawer.open()
+    }
+    fun onRegisterClickMain(){
+        val registerBottomSheet : BottomSheetDialogFragment = RegisterUserBottomSheetDialog()
+        registerBottomSheet.show(supportFragmentManager,registerBottomSheet.tag)
     }
 }
